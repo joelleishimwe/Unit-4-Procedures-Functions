c5fe4d8905e9b842a76dff0131db7286cbd62e1c
Joelle Ishimwe <36892599+joelleishimwe@users.noreply.github.com>
2020-05-21 19:25:39 -0400

    new

---
+++ RoundingDecimals/RoundingDecimals/RoundingDecimalsForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace RoundingDecimals
+{
+    partial class frmRoundingDecimals
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRounding = new System.Windows.Forms.Label();
+            this.txtDecimal = new System.Windows.Forms.TextBox();
+            this.btnRound = new System.Windows.Forms.Button();
+            this.lblDecimal = new System.Windows.Forms.Label();
+            this.nudNumRound = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumRound)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblRounding
+            // 
+            this.lblRounding.AutoSize = true;
+            this.lblRounding.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRounding.Location = new System.Drawing.Point(12, 109);
+            this.lblRounding.Name = "lblRounding";
+            this.lblRounding.Size = new System.Drawing.Size(467, 23);
+            this.lblRounding.TabIndex = 1;
+            this.lblRounding.Text = "How many decimal places are you rounding to?";
+            // 
+            // txtDecimal
+            // 
+            this.txtDecimal.Location = new System.Drawing.Point(458, 57);
+            this.txtDecimal.Name = "txtDecimal";
+            this.txtDecimal.Size = new System.Drawing.Size(100, 20);
+            this.txtDecimal.TabIndex = 3;
+            // 
+            // btnRound
+            // 
+            this.btnRound.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRound.Location = new System.Drawing.Point(474, 183);
+            this.btnRound.Name = "btnRound";
+            this.btnRound.Size = new System.Drawing.Size(96, 34);
+            this.btnRound.TabIndex = 4;
+            this.btnRound.Text = "Round";
+            this.btnRound.UseVisualStyleBackColor = true;
+            this.btnRound.Click += new System.EventHandler(this.btnRound_Click);
+            // 
+            // lblDecimal
+            // 
+            this.lblDecimal.AutoSize = true;
+            this.lblDecimal.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDecimal.Location = new System.Drawing.Point(38, 52);
+            this.lblDecimal.Name = "lblDecimal";
+            this.lblDecimal.Size = new System.Drawing.Size(365, 23);
+            this.lblDecimal.TabIndex = 5;
+            this.lblDecimal.Text = "What decimal do you want to round?";
+            // 
+            // nudNumRound
+            // 
+            this.nudNumRound.Location = new System.Drawing.Point(504, 112);
+            this.nudNumRound.Name = "nudNumRound";
+            this.nudNumRound.Size = new System.Drawing.Size(120, 20);
+            this.nudNumRound.TabIndex = 6;
+            // 
+            // frmRoundingDecimals
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 243);
+            this.Controls.Add(this.nudNumRound);
+            this.Controls.Add(this.lblDecimal);
+            this.Controls.Add(this.btnRound);
+            this.Controls.Add(this.txtDecimal);
+            this.Controls.Add(this.lblRounding);
+            this.Name = "frmRoundingDecimals";
+            this.Text = "Rounding Decimals           by:  Joelle Ishimwe";
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumRound)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRounding;
+        private System.Windows.Forms.TextBox txtDecimal;
+        private System.Windows.Forms.Button btnRound;
+        private System.Windows.Forms.Label lblDecimal;
+        private System.Windows.Forms.NumericUpDown nudNumRound;
+    }
+}
+
---
+++ RoundingDecimals/RoundingDecimals/RoundingDecimalsForm.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * Created by: Joelle Ishimwe
+ * Created on: 20-May-2020
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #33 - Rounding Program
+ * This program will accept a number with decimals 
+ * and also tell the program how many decimal places 
+ * the user want to round off to. The program then 
+ * passes By Reference the variable to be rounded off 
+ * and By Value the number of decimal points to remain 
+ * to a procedure called "RoundDecimal". Finally a 
+ * message box will display the rounded result.
+*/
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoundingDecimals
+{
+    public partial class frmRoundingDecimals : Form
+    {
+        public frmRoundingDecimals()
+        {
+            InitializeComponent();
+        }
+
+        //Function: Round Decimal 
+        //Input: ref TextBox txtRoundedBy, int numberRoundedTo
+        //Output: void
+        //Description: a fuction that rounds the decimal to number entered by the user
+        private void RoundDecimal(ref double userNumber, int userNumDecimal)
+        {
+            //multiply number by 10 to power of how many decimal points you want
+            userNumber = userNumber * Math.Pow(10, userNumDecimal);
+
+            //add 0.5
+            userNumber = userNumber + 0.5;
+
+            //get rid of numbers following the decimal(i.e.Math.Truncate)
+            userNumber = Math.Truncate(userNumber);
+
+            //move decimal point back to its original place
+            userNumber = userNumber / Math.Pow(10, userNumDecimal);
+        }
+
+        private void btnRound_Click(object sender, EventArgs e)
+        {
+            //declare variables
+            double theNumber;
+            int numDecimals;
+
+            //convert the values
+            theNumber = double.Parse(txtDecimal.Text);
+            numDecimals = (int)nudNumRound.Value;
+
+            //call the function
+            RoundDecimal(ref theNumber, numDecimals);
+
+            //display the answer in a message box
+            MessageBox.Show("The rounded decimal is:  " + theNumber, "FINAL DECIMAL");
+        }
+    }
+}
