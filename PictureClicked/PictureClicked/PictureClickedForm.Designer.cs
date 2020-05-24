d5414a4652c6bef3ffe7de3215a00e314e3792fc
Joelle Ishimwe <36892599+joelleishimwe@users.noreply.github.com>
2020-05-24 18:52:05 -0400

    new

--- PictureClicked/PictureClicked/PictureClickedForm.Designer.cs
+++ PictureClicked/PictureClicked/PictureClickedForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PictureClicked
 {
-    partial class Form1
+    partial class frmPictureClicked
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.picCard = new System.Windows.Forms.PictureBox();
+            this.lblClick = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCard
+            // 
+            this.picCard.Image = global::PictureClicked.Properties.Resources.QS;
+            this.picCard.Location = new System.Drawing.Point(75, 29);
+            this.picCard.Name = "picCard";
+            this.picCard.Size = new System.Drawing.Size(186, 227);
+            this.picCard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCard.TabIndex = 0;
+            this.picCard.TabStop = false;
+            this.picCard.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblClick
+            // 
+            this.lblClick.AutoSize = true;
+            this.lblClick.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClick.Location = new System.Drawing.Point(362, 127);
+            this.lblClick.Name = "lblClick";
+            this.lblClick.Size = new System.Drawing.Size(216, 26);
+            this.lblClick.TabIndex = 1;
+            this.lblClick.Text = "Click on the picture";
+            // 
+            // frmPictureClicked
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(697, 282);
+            this.Controls.Add(this.lblClick);
+            this.Controls.Add(this.picCard);
+            this.Name = "frmPictureClicked";
+            this.Text = "Picture Clicked                      by:    Joelle Ishimwe ";
+            ((System.ComponentModel.ISupportInitialize)(this.picCard)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picCard;
+        private System.Windows.Forms.Label lblClick;
     }
 }
 
--- PictureClicked/PictureClicked/PictureClickedForm.cs
+++ PictureClicked/PictureClicked/PictureClickedForm.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+ * Created by: Joelle Ishimwe
+ * Created on: 21-May-2020
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #33 - Clicked picture
+ * This program displays a picture box, when the 
+ * user clicks on the picture box a random number 
+ * is generated and a random picture is displayed 
+ * using a function that passes the picture by reference.
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +20,86 @@ using System.Windows.Forms;
 
 namespace PictureClicked
 {
-    public partial class Form1 : Form
+    public partial class frmPictureClicked : Form
     {
-        public Form1()
+        Random randNumberGenerator = new Random();
+        public frmPictureClicked()
         {
             InitializeComponent();
+        }
+
+        //Function: Calculate Hypotenuse
+        //Input: double aValue, double bValue
+        //Output: void
+        //Description: takes a picture box passed bt reference and a card number. 
+        //      It assigns the image in the picture box to a card with the given number
+        private void ChangedCard(ref PictureBox aPictureBox, int cardNumber)
+        {
+            if (cardNumber == 1)
+            {
+                aPictureBox.Image = Properties.Resources.Front;
+            }
+
+            else if (cardNumber == 2)
+            {
+                aPictureBox.Image = Properties.Resources.AD;
+            }
+            else if (cardNumber == 3)
+            {
+                aPictureBox.Image = Properties.Resources.AH;
+            }
+            else if (cardNumber == 4)
+            {
+                aPictureBox.Image = Properties.Resources.AC;
+            }
+            else if (cardNumber == 5)
+            {
+                aPictureBox.Image = Properties.Resources.AS;
+            }
+            else if (cardNumber == 6)
+            {
+                aPictureBox.Image = Properties.Resources.JH;
+            }
+            else if (cardNumber == 7)
+            {
+                aPictureBox.Image = Properties.Resources.JC;
+            }
+            else if (cardNumber == 8)
+            {
+                aPictureBox.Image = Properties.Resources.JD;
+            }
+            else if (cardNumber == 9)
+            {
+                aPictureBox.Image = Properties.Resources.KC;
+            }
+            else if (cardNumber == 10)
+            {
+                aPictureBox.Image = Properties.Resources.KD;
+            }
+            else if (cardNumber == 11)
+            {
+                aPictureBox.Image = Properties.Resources.KH;
+            }
+            else if (cardNumber == 12)
+            {
+                aPictureBox.Image = Properties.Resources.QC;
+            }
+            else
+            {
+                aPictureBox.Image = Properties.Resources.QD;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //declare local variable
+            int aRandomNumber;
+
+            //get a random numbers generated
+            aRandomNumber = randNumberGenerator.Next(1, 13 + 1);
+
+            //call the ChangedCard proceedure
+            ChangedCard(ref this.picCard, aRandomNumber);
         }
     }
 }
